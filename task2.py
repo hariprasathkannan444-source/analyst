b9a140b52136c77a4319ae948d9d01d5ba04526c
hari <hariprasathkannan444@gmail>
2026-06-16 11:39:13 +0530

    task2

--- task2.py
+++ task2.py
@@ -1,1 +1,11 @@
 print("hello world")
+
+#task 2 
+temp = float(input("enter temperature:"))
+
+if  temp < 20:
+    print("Weather : hot")
+elif temp <= 30:
+    print("Weather:Pleasant") 
+else:
+    print("Weather: Cold")    
