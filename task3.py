f65f987b45297cad541ee39722f8f4ee0a6b4eb9
hari <hariprasathkannan444@gmail>
2026-06-16 18:12:38 +0530

    task4

--- task3.py
+++ task3.py
@@ -6,4 +6,4 @@ num2 = float(input("Enter second number: "))
 print("Addition =", num1 + num2)
 print("Subtraction =", num1 - num2)
 print("Multiplication =", num1 * num2)
-print("Division =", num1 / num2)55
+print("Division =", num1 / num2)
---
+++ task4.py
@@ -0,0 +1,12 @@
+print("hello chennai")
+
+fahrenheit = float(input("Enter Fahrenheit: "))
+
+celsius = (fahrenheit - 32) * 5 / 9
+
+print("Celsius =", celsius)
+
+
+
+
+
